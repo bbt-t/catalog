85426262430535137546cb64465a41722f0cca4b
bbt-t <zlukcss@gmail.com>
2023-08-11 12:20:39 -0300

    upd db models

--- catalog/models.py
+++ catalog/models.py
@@ -23,46 +23,68 @@ class Feedback(models.Model):
         editable=False,
         auto_now_add=True,
         db_comment="Date and time when the question was created",
+        verbose_name="Создано",
     )
 
     class Meta:
         db_table_comment = "Users feedback"
+        ordering = "create_at",
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, help_text="Category name")
-    description = models.CharField(max_length=512, help_text="Category description")
+    name = models.CharField(
+        max_length=128,
+        help_text="Category name",
+    )
+    description = models.CharField(
+        max_length=512,
+        help_text="Category description",
+        verbose_name="Описание",
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table_comment = "Product categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
-    description = models.CharField(max_length=512)
-    price = models.PositiveBigIntegerField(blank=True, null=True)
+    description = models.CharField(
+        max_length=512,
+        verbose_name="Описание",
+    )
+    price = models.PositiveBigIntegerField(
+        blank=True,
+        null=True,
+        verbose_name="Цена",
+    )
     image_preview = models.ImageField(
         upload_to="images/",
         help_text="Images are here -> images/",
         null=True,
         blank=True,
+        verbose_name="Фото товара",
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
         help_text="This is a foreign key of the category",
+        verbose_name="Категория товара",
     )
     create_at = models.DateTimeField(
         editable=False,
         auto_now_add=True,
         db_comment="Date and time when the product was created",
+        verbose_name="Создано",
     )
     update_at = models.DateTimeField(
         auto_now=True,
         db_comment="Date and time when the product was updated",
+        verbose_name="Обновлено",
     )
 
     def __str__(self):
@@ -70,15 +92,19 @@ class Product(models.Model):
 
     class Meta:
         db_table_comment = "Goods"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = "price",
 
 
 class ContactDetails(models.Model):
-    country = models.CharField(max_length=64)
-    inn = models.SlugField(max_length=64)
-    address = models.CharField(max_length=256)
+    country = models.CharField(max_length=64, verbose_name="Номер телефона")
+    inn = models.SlugField(max_length=64, verbose_name="ИНН")
+    address = models.CharField(max_length=256, verbose_name="Адрес")
 
     def __str__(self):
         return self.address
 
     class Meta:
         db_table_comment = "Contact details"
+        verbose_name = "Контакты"
