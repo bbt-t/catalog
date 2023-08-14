b77d63bf5dbd20016cbbd3013c85f5e57984ef44
bbt-t <zlukcss@gmail.com>
2023-08-14 12:29:38 -0300

    creating a new prod record through the site page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from catalog.views import HomePageView, ContactsView, AboutProduct
+from catalog.views import (
+    HomePageView,
+    ContactsView,
+    AboutProduct,
+    AddProduct,
+    accept_add_product,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="homepage"),
     path("product/<int:id>/", AboutProduct.as_view(), name="product-detail"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("add/", AddProduct.as_view(), name="add_product"),
+    path("ok", accept_add_product, name="ok"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,12 @@
 from logging import warning as log
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 
-from catalog.models import Feedback, Product, ContactDetails
+from catalog.models import Feedback, Product, ContactDetails, Category
 
 
 class HomePageView(TemplateView):
@@ -57,3 +59,45 @@ class ContactsView(View):
             "contacts": ContactDetails.objects.get(),
         }
         return render(request, self.template_name, context)
+
+
+class AddProduct(View):
+    template_name = "../templates/catalog/add_product.html"
+
+    def post(self, request, *args, **kwargs):
+        """
+        Get info for save a new Product.
+        """
+        _, name, category_id, price, description = request.POST.values()
+        selected_category = Category.objects.get(pk=category_id)
+        try:
+            new_entry = Product(
+                category=selected_category,
+                name=name,
+                description=description,
+                price=price,
+                image_preview=request.FILES.getlist("product_image")[0],
+            )
+        except Exception as e:
+            print(e)
+        else:
+            new_entry.save()
+            return HttpResponseRedirect(reverse("ok"))
+
+        return render(request, self.template_name)
+
+    def get(self, request, *args, **kwargs):
+        """
+        GET request.
+        """
+        context = {
+            "categories": Category.objects.all(),
+        }
+        return render(request, self.template_name, context)
+
+
+def accept_add_product(request):
+    """
+    If the prod addition was successful.
+    """
+    return render(request, "../templates/catalog/accept_add.html")
